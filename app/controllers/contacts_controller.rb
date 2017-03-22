ce106cd4dd6c7537810f7f08939de46aa8ceef92
kazankarpov <kazankarpov@gmail.com>
2017-03-22 20:14:40 +0300

    Start

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,16 @@
+class ContactsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  @contact = Contact.new(contact_params)
+  @contact.save
+  end
+  
+    private
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
+end
