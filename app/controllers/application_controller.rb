a58ea66dad77bd604f7c9d6d38a11e33217f0a0c
kazankarpov <kazankarpov@gmail.com>
2017-04-02 11:38:32 +0300

    permit username for signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+before_filter :configure_permited_parametrs, if: :devise_controller?
+
+private
+  def confuigure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username 
+    
+    end
 end
