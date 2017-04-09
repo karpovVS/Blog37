f0fc3dbb0ec93e73ef5d97ea37b5b7adf27e14aa
karpovVS <kazankarpov@gmail.com>
2017-04-09 18:03:37 +0300

    update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,5 +3,13 @@ FactoryGirl.define do
 title "Article title"
 text "Article text"
 
+#создаем фабрику с названием article_with_comments 
+factory :article_with_comments do
+# после создания article
+after :create do |article, evaluator|
+# создаём список из 3-х комментов
+create_list :comment, 3, article: article
+	end
+end
 end
 end
