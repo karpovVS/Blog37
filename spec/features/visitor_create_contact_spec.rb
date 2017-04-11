a9967f6318c53c0b71e362620ba3d3e27eb0b5bc
karpovVS <kazankarpov@gmail.com>
2017-04-11 15:20:21 +0300

    update tests (allow  guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,16 @@ require "spec_helper"
 feature "Contact Creation"  do 
 scenario "allows aces to contacts page " do
 	visit '/contacts'
-	expect(page).to have_content 'Contact us'	
+	expect(page).to have_content I18n.t('contacts.contact_us')
+end
+  scenario "allows a guest to create contact" do
+visit '/contacts'
+
+fill_in :contact_email, :with => 'user@examle.com'
+fill_in :contact_message, :with => 'something'
+
+click_button 'Send message'
+
+expect(page).to have_content 'Fuck you'
 end
 end
