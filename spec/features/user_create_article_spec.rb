95c40c223fa3201b4c64b41a7eb804acc3095d11
karpovVS <kazankarpov@gmail.com>
2017-04-12 00:30:47 +0300

    update old error

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
-feature "Article Creation"  do 
+feature "Article Creation" do 
   before(:all) do
   sign_up
   end
 
 scenario "allows user to visit new article page" do
   visit new_article_path 
-  expect(page).to have_content 'New article'
+  expect(page).to have_content 'New Article'
   end
 end
